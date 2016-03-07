d7665f5984b317c3e90536a0e8c0966077512c02
t3rr0r <mail@t3rr0r.com>
2016-03-07 10:25:30 -0500

    fixed query param serialization

--- src/request.js
+++ src/request.js
@@ -8,14 +8,49 @@
 
     'use strict';
 
-    function serialize(obj) {
-	var str = [];
-	for(var p in obj)
-	    if (obj.hasOwnProperty(p)) {
-		str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+    function buildParams(prefix, obj, add) {
+	var name, i, l, rbracket;
+	rbracket = /\[\]$/;
+	if (obj instanceof Array) {
+	    for (i = 0, l = obj.length; i < l; i++) {
+		if (rbracket.test(prefix)) {
+		    add(prefix, obj[i]);
+		} else {
+		    buildParams(prefix + '[' + ( typeof obj[i] === 'object' ? i : '' ) + ']', obj[i], add);
+		}
 	    }
-	return str.join('&');
+	} else if (typeof obj == 'object') {
+	    // Serialize object item.
+	    for (name in obj) {
+		buildParams(prefix + '[' + name + ']', obj[ name ], add);
+	    }
+	} else {
+	    // Serialize scalar item.
+	    add(prefix, obj);
+	}
     }
+
+    var objectToQueryString = function (a) {
+	var prefix, s, add, name, r20, output;
+	s = [];
+	r20 = /%20/g;
+	add = function (key, value) {
+	    // If value is a function, invoke it and return its value
+	    value = ( typeof value == 'function' ) ? value() : ( value == null ? '' : value );
+	    s[ s.length ] = encodeURIComponent(key) + '=' + encodeURIComponent(value);
+	};
+	if (a instanceof Array) {
+	    for (name in a) {
+		add(name, a[name]);
+	    }
+	} else {
+	    for (prefix in a) {
+		buildParams(prefix, a[ prefix ], add);
+	    }
+	}
+	output = s.join('&').replace(r20, '+');
+	return output;
+    };
 
     var parse = function (req) {
 	var result;
@@ -82,7 +117,7 @@
 	    return xhr('HEAD', url);
 	},
 	get: function(url, data) {
-	    if (data) url += '?' + serialize(data);
+	    if (data) url += '?' + objectToQueryString(data);
 	    return xhr('GET', url);
 	},
 	put: function(url, data) {
@@ -92,7 +127,7 @@
 	    return xhr('POST', url, data);
 	},
 	del: function(url, data) {
-	    if (data) url += '?' + serialize(data);
+	    if (data) url += '?' + objectToQueryString(data);
 	    return xhr('DELETE', url);
 	}
     };
